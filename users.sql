dfab03f8f9e056b185e9dae2220de66f77645554
Nivedha <nivedhaj1997@gmail.com>
2019-12-26 16:28:02 +0530

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,3 +1,6 @@
 create table taskuser
 (task_id number,user_name varchar2(30),e_mail varchar2(30),mail_password varchar2(10));
 insert into taskuser(task_id,user_name,e_mail,mail_password) values (1,'nivedha','nivedhaj1997@gmail.com','nivedhaj');
+insert into taskuser(task_id,user_name,e_mail,mail_password) values (2,'kavitha','kavithaj1997@gmail.com','kavithaj');
+insert into taskuser(task_id,user_name,e_mail,mail_password) values (3,'xxxx','xxxj1997@gmail.com','nivedhaj');
+select * from taskuser;
