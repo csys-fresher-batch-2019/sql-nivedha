857167d805b47c39fd09917b8d09b9ddce6ab680
Nivedha <nivedhaj1997@gmail.com>
2019-12-26 19:44:12 +0530

    Update task.sql

--- task.sql
+++ task.sql
@@ -11,14 +11,14 @@ task_cd date,
 task_status varchar(30),
 constraint task_id_pk primary key(task_id),
 constraint task_name_uq unique(task_name),
-constraint task_status_ck check(task_status in('completed','notcompleted'),
-constraint task_priority_ck check(task_priority in('H','M','L')
+constraint task_status_ck check(task_status in('completed','notcompleted')),
+constraint task_priority_ck check(task_priority in('H','M','L'))
 );
 insert into task
 (
 task_id, task_name, task_priority,task_created,task_modified, task_by, task_dd, task_cd, task_status) values
 (1,'java','H',
-task_created=systimestamp,task_modified=systimestamp,
+task_created=sys timestamp,task_modified=sys timestamp,
 'nivedha',
 to_date('27-12-2019','dd-mm-yyyy'),to_date('26-12-2019','dd-mm-yyyy'),'completed'
 );
@@ -27,7 +27,7 @@ insert into task
 (
 task_id, task_name, task_priority,task_created,task_modified, task_by, task_dd, task_cd, task_status) values
 (2,'sql','L',
-task_created=systimestamp,task_modified=systimestamp,
+task_created=sys timestamp,task_modified=sys timestamp,
 'sathvika',
 to_date('28-12-2019','dd-mm-yyyy'),to_date('29-12-2019','dd-mm-yyyy','notcompleted')
 );
