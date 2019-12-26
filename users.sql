0a07004acee20a55e6b5a8ee3c16eecbac36beee
Nivedha <nivedhaj1997@gmail.com>
2019-12-26 16:03:24 +0530

    Create users.sql

---
+++ users.sql
@@ -0,0 +1,3 @@
+create table taskuser
+(task_id number,user_name varchar2(30),e_mail varchar2(30),mail_password varchar2(10));
+insert into taskuser(task_id,user_name,e_mail,mail_password) values (1,'nivedha','nivedhaj1997@gmail.com','nivedhaj');
