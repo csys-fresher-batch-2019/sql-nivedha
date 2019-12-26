9c97fe0293eb7f219104232574e3d6af10458ffd
Nivedha <nivedhaj1997@gmail.com>
2019-12-26 11:32:29 +0530

    Update task.sql

--- task.sql
+++ task.sql
@@ -8,23 +8,19 @@ task_cd date,
   task_status varchar(30),
 constraint task_id_pk primary key(task_id),
 constraint task_name_uq unique(task_name),
- constraint task_status_ck check(task_status in('c','nc') 
+ constraint task_status_ck check(task_status in('completed','notcompleted') 
 );
 insert into task
 (
 task_id, task_name, task_by, task_dd, task_cd) values
-(1,'java','nivedha',to_date('27-12-2019','dd-mm-yyyy'),to_date('26-12-2019','dd-mm-yyyy','c')
+(1,'java','nivedha',to_date('27-12-2019','dd-mm-yyyy'),to_date('26-12-2019','dd-mm-yyyy','completed')
 );
+
 insert into task
 (
 task_id, task_name, task_by, task_dd, task_cd) values
-(2,'oracle','kavitha',to_date('25-12-2019','dd-mm-yyyy'),to_date('24-12-2019','dd-mm-yyyy','c')
+(2,'sql','sathvika',to_date('28-12-2019','dd-mm-yyyy'),to_date('29-12-2019','dd-mm-yyyy','notcompleted')
 );
-insert into task
-(
-task_id, task_name, task_by, task_dd, task_cd) values
-(3,'sql','sathvika',to_date('28-12-2019','dd-mm-yyyy'),to_date('29-12-2019','dd-mm-yyyy','nc')
-);
-select * from task where task_status='c';
+select * from task where task_status='completed';
 
 
