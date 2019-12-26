9e0382fd2a0b2847b4ca03b04ed243e30409df01
Nivedha <nivedhaj1997@gmail.com>
2019-12-26 16:54:17 +0530

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,6 +1,7 @@
 create table taskuser
-(task_id number,user_name varchar2(30) not null,e_mail varchar2(30),mail_password varchar2(10) not null)
-constraint task_id_pk primary key(task_id),
+(task_id number,user_id number,user_name varchar2(30) not null,e_mail varchar2(30),mail_password varchar2(10) not null),
+constraint task_id_fk foreign key(task_id) references task(task_id),
+constraint user_id_pk primary key(task_id),
 constraint e_mail_uq unique(e_mail)
 ;
 insert into taskuser(task_id,user_name,e_mail,mail_password) values (1,'nivedha','nivedhaj1997@gmail.com','nivedhaj');
